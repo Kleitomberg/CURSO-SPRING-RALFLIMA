ebfa80f2dead3227c2eccb77d0ca53324ee1130c
Kleitomberg <51670381+Kleitomberg@users.noreply.github.com>
2023-01-31 21:54:50 -0300

    handle exeption

---
+++ src/main/java/br/com/projeto/api/Validation/Validacao.java
@@ -0,0 +1,39 @@
+package br.com.projeto.api.Validation;
+
+// Importar Map e HashMap
+import java.util.HashMap;
+import java.util.Map;
+
+// Importar http, validation, bind, context e servlet
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class Validacao extends ResponseEntityExceptionHandler{
+
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        // TODO Auto-generated method stub
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+
+    }
+
+}
